ba2a17e23da406a009aafe3217a3f1271ee7ef34
unknown <manikttmg11@gmail.com>
2024-08-02 20:25:27 +0545

    asd

--- lib/views/ScreenViews/profile_page.dart
+++ lib/views/ScreenViews/profile_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
@@ -10,9 +11,84 @@ class ProfilePage extends StatefulWidget {
 class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("ProfilePage1"),
+    return Scaffold(
+      body: Column(
+        children: [
+          const SizedBox(height: 10,),
+          Center(
+            child: ClipOval(
+              child: Image.asset(
+                "assets/images/testimage1.jpg", // Replace with a valid image URL
+                width: 140, // Increased size for better visibility
+                height: 140,
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+
+          const SizedBox(height: 10,),
+
+          Text(
+              "Username",
+              style: GoogleFonts.lato(
+                fontWeight: FontWeight.w800,
+                fontSize: 22,
+                color: Colors.black,
+              ),
+            ),
+
+            
+
+           Padding(
+             padding: const EdgeInsets.all(40),
+             child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Column(
+                  children: [
+                    Text(
+                      "146",
+                      style: GoogleFonts.lato(
+                      fontWeight: FontWeight.w800,
+                      fontSize: 22,
+                      color: Colors.black,
+                    ),
+                  ),
+                    Text(
+                      "Followers",
+                      style: GoogleFonts.lato(
+                      fontWeight: FontWeight.w800,
+                      fontSize: 22,
+                      color: Colors.black,
+                    ),
+                  ),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Text(
+                      "146",
+                      style: GoogleFonts.lato(
+                      fontWeight: FontWeight.w800,
+                      fontSize: 22,
+                      color: Colors.black,
+                    ),
+                  ),
+                    Text(
+                      "Followers",
+                      style: GoogleFonts.lato(
+                      fontWeight: FontWeight.w800,
+                      fontSize: 22,
+                      color: Colors.black,
+                    ),
+                  ),
+                  ],
+                )
+              ],
+                       ),
+           )
+        ],
       ),
     );
   }
